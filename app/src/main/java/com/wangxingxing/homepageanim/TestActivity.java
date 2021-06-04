65aaa2093ce21a0189ed02303104b355a02126d4
WangXingxing <1099420259@qq.com>
2021-06-04 11:34:28 +0800

    update

--- app/src/main/java/com/wangxingxing/homepageanim/TestActivity.java
+++ app/src/main/java/com/wangxingxing/homepageanim/TestActivity.java
@@ -1,14 +1,22 @@
 package com.wangxingxing.homepageanim;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.Animator;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
 import com.hoanganhtuan95ptit.library.MeteorView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Method;
 
 
 /**
@@ -29,26 +37,26 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
 
-        mMeteorView = findViewById(R.id.img_meteor0);
-        mMeteorView.setImageResource(R.drawable.icon_meteor);
+//        mMeteorView = findViewById(R.id.img_meteor0);
+//        mMeteorView.setImageResource(R.drawable.icon_meteor_new);
 
-        mButtonStart = findViewById(R.id.btn_start);
-        mButtonStart.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnAnim();
-            }
-        });
-
-        mButtonTest = findViewById(R.id.btn_test);
-
-        mButtonOpenGL = findViewById(R.id.btn_opengl);
-        mButtonOpenGL.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(TestActivity.this, OpenGLActivity.class));
-            }
-        });
+//        mButtonStart = findViewById(R.id.btn_start);
+//        mButtonStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                btnAnim();
+//            }
+//        });
+//
+//        mButtonTest = findViewById(R.id.btn_test);
+//
+//        mButtonOpenGL = findViewById(R.id.btn_opengl);
+//        mButtonOpenGL.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(TestActivity.this, OpenGLActivity.class));
+//            }
+//        });
     }
 
     private void btnAnim() {
@@ -77,5 +85,52 @@ public class TestActivity extends AppCompatActivity {
                     }
                 })
                 .start();
+    }
+
+//    @Override
+//    public Resources getResources() {
+//        Resources res = super.getResources();
+//        Configuration config = new Configuration();
+//        config.setToDefaults();
+//        res.updateConfiguration(config, res.getDisplayMetrics());
+//        return res;
+//    }
+
+
+    @Override
+    public Resources getResources() {
+        Resources res = super.getResources();
+        if (res != null && res.getConfiguration().fontScale != 1.0f) {
+            res.getConfiguration().fontScale = 1.0f; //恢复默认的[字体大小]
+            res.getConfiguration().densityDpi = getDefaultDisplayDensity(); //恢复默认的[显示大小]
+            res.updateConfiguration(res.getConfiguration(), res.getDisplayMetrics());
+        }
+        return super.getResources();
+    }
+
+    private int getDefaultDisplayDensity() {
+        try {
+            Class<?> clazz = Class.forName("android.view.WindowManagerGlobal");
+            Method method = clazz.getMethod("getWindowManagerService");
+            method.setAccessible(true);
+            Object iwm = method.invoke(clazz);
+            Method getInitialDisplayDensity  = iwm.getClass().getMethod("getInitialDisplayDensity", int.class);
+            getInitialDisplayDensity.setAccessible(true);
+            int densityDpi = (int) getInitialDisplayDensity.invoke(iwm, Display.DEFAULT_DISPLAY);
+            return densityDpi;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull @NotNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.fontScale != 1.0f) {
+            //刷新
+            getResources();
+        }
+        super.onConfigurationChanged(newConfig);
     }
 }
