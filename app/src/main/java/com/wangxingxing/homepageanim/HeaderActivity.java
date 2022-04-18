4e11af6b40d3d5be4602dbbf13aee8bd117d619c
WangXingxing <1099420259@qq.com>
2022-04-18 15:05:28 +0800

    update

---
+++ app/src/main/java/com/wangxingxing/homepageanim/HeaderActivity.java
@@ -0,0 +1,14 @@
+package com.wangxingxing.homepageanim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HeaderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header);
+    }
+}
